bf65e260fbc2a05f9fdd6aaa936db221384e6565
Samir Founou <samir.founou@avizzeo.com>
2020-05-12 14:46:03 +0200

    MAJ

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Allow to redirect to home
+     * 
+     * @Route("/", name="home")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
